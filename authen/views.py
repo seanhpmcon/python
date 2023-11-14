2527f8e05370bdfbe3531bc524c7ca0a2f990644
Sean Harricharan <seanh@pmcon.co.tt>
2023-11-14 08:39:14 -0400

    modified views of the authen package to handle auth funstions

--- authen/views.py
+++ authen/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        pass
+    if request.GET.get('login'):
+        fail = True
+    else:
+        fail = False
+    # template = loader.get_template('login.html')
+    # return HttpResponse(template.render({'fail': 'fail'}, request))
+    return render(request, 'login.html', {'fail': fail})
+
+@csrf_exempt
+def logout(request):
+    del request.session['ldap_name']
+    return redirect('/attendance/login')
