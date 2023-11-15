9ee6dea1972c1bfcd92847627d789203f135d1aa
Sean Harricharan <seanh@pmcon.co.tt>
2023-11-15 07:53:02 -0400

    added base file

--- attendance/forms.py
+++ attendance/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.core.validators import FileExtensionValidator
 from django.forms.utils import ErrorList
+from django.forms import FileInput
 
 # This class is to instantiate a form to take in a file field.
 class UploadFileForm(forms.Form):
-    file = forms.FileField(validators=[FileExtensionValidator(['xlsx', 'xls'])])
+    file = forms.FileField(validators=[FileExtensionValidator(['xlsx', 'xls'])], widget=FileInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
     
 # This class is for returning a specific error when a different file type is added.
 class BootstrapErrorList(ErrorList):
